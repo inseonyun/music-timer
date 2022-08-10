1c12ee5befa29217417cd48fcf8e9161a957d943
inseonyun <yuninseon@naver.com>
2022-08-10 21:37:50 +0900

    Notification 클릭 시 onResume를 불러오게 되는데, 기존 onResume에서 Enabled 조정 하던 것을 onStart에서 하도록 함

--- music-timer/app/src/main/java/com/media_music_timer/Fragment_home.kt
+++ music-timer/app/src/main/java/com/media_music_timer/Fragment_home.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,9 +26,8 @@ class Fragment_home : Fragment() {
 
     var intent: Intent? = null
 
-    override fun onResume() {
-        super.onResume()
-
+    override fun onStart() {
+        super.onStart()
         AllEnabled(true)
     }
 
